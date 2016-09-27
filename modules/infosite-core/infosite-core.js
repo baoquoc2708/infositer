d2f107aa86994aa3e0a030dc5546a9ca643170bc
Amit Parikh <AParikh@medscape.net>
2016-09-27 16:37:36 -0400

    article load back to react

--- modules/infosite-core/infosite-core.js
+++ modules/infosite-core/infosite-core.js
@@ -12,44 +12,26 @@ export default {
 		feature2: false
 	},
 	infositeRouting(){
-		let articleName = window.location.pathname.split("/").pop();
-		if(articleName == 'article-1'){
-			$('.article').children().remove();
-			$('.article').load('/article/article-1.html');
-		}
-		$('.menu-list a, .sub-menu-list a').on('click', function(event) {
-			if(event.target.href.indexOf('#') > -1){
-				$(this).removeAttr('target');
-				console.log(event.target.href);
-			} else if(event.target.href.indexOf(window.location.origin) == -1){
-				console.log(event.target.href);
-				console.log(window.location.origin);
+		
 
-			} else {
-				event.preventDefault();
-				console.log(event.target.href);
-			}
+		$('.infosite-footer').on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			//window.history.pushState(null, null, 'http://localhost:8080/article-1');
+			//console.log("here");
+			//$('.article').load('/article/article-1.html',
+			//	function(){
+				/* Stuff to do after the page is loaded */
+			//});
+			
+		});
+		const originUrl = window.location.origin + '/infosite/infositeR_POC';
+		$('.medscape-link').on('click', function(event) {
+			event.preventDefault();
+			/* Act on the event */
+			window.history.pushState(null, null, originUrl);
+			console.log("back");
+			$('.article').children().remove();
 		});
 	}
-	/*loadArticle(event){
-        const originUrl = window.location.origin + '/infosite/infositeR_POC';
-        event.stopPropagation();
-        if(event.target.href.indexOf(window.location.origin) == -1){
-            return;
-        }
-        let pageUrl = event.target.href;
-        const dirName = '/articles/'
-        event.preventDefault();
-        if(event.target.href.indexOf('#') > -1){
-            let hashUrl = originUrl +'#'+ pageUrl.split('#')[1];
-            window.history.pushState(null, null, hashUrl);
-        } else {
-            window.history.pushState(null, null, originUrl + dirName + $(event.target).attr('href'));
-            $('.article').load(originUrl + dirName + $(event.target).attr('href'));
-        }    
-    },
-    listAction(event){
-        event.stopPropagation();
-        console.log(event.target);
-    },*/
 };
